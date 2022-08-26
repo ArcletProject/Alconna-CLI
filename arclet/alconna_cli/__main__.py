828c4bd216e4fd2e6898485b0dcfb9eb7f53f27b
RF-Tar-Railt <3165388245@qq.com>
2022-08-26 12:03:33 +0800

    0.2.1

--- arclet/alconna_cli/__main__.py
+++ arclet/alconna_cli/__main__.py
@@ -1,6 +1,6 @@
 """Alconna 命令行入口"""
 
-from arclet.alconna_cli.commandline import alc_cli
+from arclet.alconna_cli import alc_cli
 
 if __name__ == "__main__":
     main = alc_cli.main
--- arclet/alconna_cli/commandline.py
+++
@@ -1,6 +0,0 @@
-from clilte import CommandLine
-from arclet.alconna import alconna_version
-from .plugins import Create, Cache, Lang, Analysis, Using
-
-alc_cli = CommandLine("alconna", "Alconna-CLI", alconna_version)
-alc_cli.add(Create, Cache, Lang, Analysis, Using)
